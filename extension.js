530bb7665c643af460dd9640a2e0399a69cfc1f9
Gniourf <gniourfgniourf@gmail.com>
2021-10-05 12:02:07 +0200

    fixed stupid errors in previous commit

--- extension.js
+++ extension.js
@@ -22,131 +22,136 @@ const Shell = imports.gi.Shell;
 const Mainloop = imports.mainloop;
 const PopupMenu = imports.ui.popupMenu;
 const Clutter = imports.gi.Clutter;
+const GObject = imports.gi.GObject;
 
 let _uptime_indicator_object = null;
 
-const UptimeIndicator = GObject.registerClass({
-    GTypeName: "UptimeIndicator",
-},
+const UptimeIndicator = GObject.registerClass(
+    {
+        GTypeName: "UptimeIndicator"
+    },
     class UptimeIndicator extends PanelMenu.Button
-{
-    _init()
     {
-        this._timeout = null;
-        this._refresh_rate = 1;
-        // this._change_timeout_loop = false;
-        this._started = null;
+        _init()
+        {
+            // this._timeout = null;
+            // this._refresh_rate = 1;
+            // // this._change_timeout_loop = false;
+            // this._started = null;
 
-        this.parent(0.0, "Uptime Indicator", false);
+            // this.parent(0.0, "Uptime Indicator", false);
+            super._init(0.0, "Uptime Indicator", false);
 
-        this.buttonText = new St.Label({
-            name: "uptime-indicator-buttonText",
-            y_align: Clutter.ActorAlign.CENTER
-        });
-        this.actor.add_actor(this.buttonText);
+            this.buttonText = new St.Label({
+                name: "uptime-indicator-buttonText",
+                y_align: Clutter.ActorAlign.CENTER
+            });
+            this.actor.add_actor(this.buttonText);
 
-        /* Find starting date and */
-        let timestamp = this._get_timestamps()[0];
-        let date = new Date();
-        date.setTime(date - timestamp*1000)
-        this._started = date.toLocaleString();
-        /* and prepare menu */
-        this._mymenutitle = new PopupMenu.PopupMenuItem(this._started, { reactive: false });
-        this.menu.addMenuItem(this._mymenutitle);
+            /* Find starting date and */
+            let timestamp = this._get_timestamps()[0];
+            let date = new Date();
+            date.setTime(date - timestamp*1000)
+            this._started = date.toLocaleString();
+            /* and prepare menu */
+            this._mymenutitle = new PopupMenu.PopupMenuItem(this._started, { reactive: false });
+            this.menu.addMenuItem(this._mymenutitle);
 
-        this.actor.connect('button-press-event', this._refresh.bind(this));
-        this.actor.connect('key-press-event', this._refresh.bind(this));
+            this.actor.connect('button-press-event', this._refresh.bind(this));
+            this.actor.connect('key-press-event', this._refresh.bind(this));
 
-        this._set_refresh_rate(1)
-        this._change_timeoutloop = true;
-        this._refresh();
-    }
-
-    _get_timestamps()
-    {
-        return Shell.get_file_contents_utf8_sync('/proc/uptime').split(" ");
-    }
-
-    _refresh()
-    {
-        let text = this._update_uptime();
-        this.buttonText.set_text(text)
-        if(this._change_timeoutloop) {
-            this._remove_timeout();
-            this._timeout = Mainloop.timeout_add_seconds(this._refresh_rate, this._refresh.bind(this));
-            this._change_timeoutloop = false;
-            return false;
-        }
-        return true;
-    }
-
-    _set_refresh_rate(refresh_rate)
-    {
-        if(this._refresh_rate != refresh_rate) {
-            this._refresh_rate = refresh_rate;
+            this._set_refresh_rate(1)
             this._change_timeoutloop = true;
+            this._refresh();
         }
-    }
 
-    _remove_timeout()
-    {
-        if(this._timeout) {
-            Mainloop.source_remove(this._timeout);
-            this._timeout = null;
+        _get_timestamps()
+        {
+            return Shell.get_file_contents_utf8_sync('/proc/uptime').split(" ");
         }
-    }
 
-    _update_uptime()
-    {
-        let timestamps_s = this._get_timestamps()[0];
-        let minutes = Math.floor((timestamps_s / 60) % 60);
-        let hours = Math.floor((timestamps_s / 3600) % 24);
-        let days = Math.floor((timestamps_s / 86400) % 365);
-        let years = Math.floor(timestamps_s / 31536000);
-        let label_text = "?";
-        if(years>0) {
-            label_text = years + "Y" + days + "D";
-            /* Come back next year */
-            this._set_refresh_rate(31536000 - (timestamps_s) % 31536000);
-        }
-        else if(days>99) {
-            label_text = days + "D";
-            /* Come back next day */
-            this._set_refresh_rate(86400 - (timestamps_s % 86400))
-        }
-        else if(days>0) {
-            if(hours < 10) {
-                hours = "0" + hours;
+        _refresh()
+        {
+            let text = this._update_uptime();
+            this.buttonText.set_text(text)
+            if(this._change_timeoutloop) {
+                this._remove_timeout();
+                this._timeout = Mainloop.timeout_add_seconds(this._refresh_rate, this._refresh.bind(this));
+                this._change_timeoutloop = false;
+                return false;
             }
-            label_text = days + "D" + hours + "h";
-            /* Come back next hour */
-            this._set_refresh_rate(3600 - (timestamps_s % 3600))
+            return true;
         }
-        else {
-            if(minutes < 10) {
-                minutes = "0" + minutes;
-            }
-            label_text = hours + ":" + minutes;
-            /* Come back next minute */
-            this._set_refresh_rate(60 - (timestamps_s % 60));
-        }
-        return label_text;
-    }
 
-    destroy()
-    {
-        this._remove_timeout();
-        this.parent();
+        _set_refresh_rate(refresh_rate)
+        {
+            if(this._refresh_rate != refresh_rate) {
+                this._refresh_rate = refresh_rate;
+                this._change_timeoutloop = true;
+            }
+        }
+
+        _remove_timeout()
+        {
+            if(this._timeout) {
+                Mainloop.source_remove(this._timeout);
+                this._timeout = null;
+            }
+        }
+
+        _update_uptime()
+        {
+            let timestamps_s = this._get_timestamps()[0];
+            let minutes = Math.floor((timestamps_s / 60) % 60);
+            let hours = Math.floor((timestamps_s / 3600) % 24);
+            let days = Math.floor((timestamps_s / 86400) % 365);
+            let years = Math.floor(timestamps_s / 31536000);
+            let label_text = "?";
+            if(years>0) {
+                label_text = years + "Y" + days + "D";
+                /* Come back next year */
+                this._set_refresh_rate(31536000 - (timestamps_s) % 31536000);
+            }
+            else if(days>99) {
+                label_text = days + "D";
+                /* Come back next day */
+                this._set_refresh_rate(86400 - (timestamps_s % 86400))
+            }
+            else if(days>0) {
+                if(hours < 10) {
+                    hours = "0" + hours;
+                }
+                label_text = days + "D" + hours + "h";
+                /* Come back next hour */
+                this._set_refresh_rate(3600 - (timestamps_s % 3600))
+            }
+            else {
+                if(minutes < 10) {
+                    minutes = "0" + minutes;
+                }
+                label_text = hours + ":" + minutes;
+                /* Come back next minute */
+                this._set_refresh_rate(60 - (timestamps_s % 60));
+            }
+            return label_text;
+        }
+
+        destroy()
+        {
+            this._remove_timeout();
+            // this.parent();
+            super.destroy();
+        }
     }
-});
+);
 
 // Init function
-init(metadata)
+function init(metadata)
 {
 }
 
 // Enable function
-enable()
+function enable()
 {
     _uptime_indicator_object = new UptimeIndicator;
     if(_uptime_indicator_object) {
@@ -155,7 +160,7 @@ enable()
 }
 
 // Disable function
-disable()
+function disable()
 {
     if(_uptime_indicator_object) {
         _uptime_indicator_object.destroy();
